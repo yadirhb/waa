b6ce693395a6c30e3a9f44b67dec2f5c6f2b0937
Yadir Hernandez Batista <yadirhb@gmail.com>
2019-11-07 15:57:55 -0600

    Finishing lab7

--- Lab7/EmployeeValidDemo/src/main/java/edu/mum/configuration/Dispatcher.java
+++ Lab7/EmployeeValidDemo/src/main/java/edu/mum/configuration/Dispatcher.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
@@ -37,6 +38,13 @@ public class Dispatcher extends WebMvcConfigurerAdapter {
 		InternalResourceViewResolver bean = new InternalResourceViewResolver();
 		bean.setPrefix("/WEB-INF/jsp/");
 		bean.setSuffix(".jsp");
+		return bean;
+	}
+	
+	@Bean
+	public AcceptHeaderLocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver bean = new AcceptHeaderLocaleResolver();
+		
 		return bean;
 	}
 
